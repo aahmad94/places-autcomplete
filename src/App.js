96d4ecf3e7336f5a74e942a814fbb69eab011f64
aahmad94 <aka5vm@virginia.edu>
2018-05-17 18:53:21 -0700

    initialize repo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: [],
+      selected: ""
+    }
+  }
+
+  async countriesAPI(e) {
+    const url = `/api/countries?q=${e.target.value}`;
+    const resp = await axios.get(url);
+    this.setState({ countries: resp.data })
+  }
+
+  handleSelect(name) {
+    this.setState({
+      selected: name
+    });
+  }
+  
   render() {
     return (
       <div className="App">
@@ -10,6 +31,28 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div>Currently selected country: { this.state.selected } </div>
+        <div>Search</div>
+        <input
+          placeholder="Search..."
+          onChange={e => this.countriesAPI(e)}
+        >
+        </input>
+        
+        <ul>
+          {
+            this.state.countries.map(obj => {
+            return (<li
+                      onClick={() => this.handleSelect(obj.name)}
+                      key={obj.name}>
+              {
+                obj.name
+              }  
+            </li>)
+            })
+          }
+        </ul>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
